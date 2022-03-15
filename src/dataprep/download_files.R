4cd74137b1a3aaa09c2582d09430999351fc4dbe
Romeehoeks <98810281+Romeehoeks@users.noreply.github.com>
2022-03-15 19:37:17 +0100

    Update download_files.R
    Add the correct library in the code

--- src/dataprep/download_files.R
+++ src/dataprep/download_files.R
@@ -1,8 +1,10 @@
 #Download the files through google drive
+library(googledrive)
 data_id <-"1p-4gvEglcpfqD9qkLsU0oAsCDOBKgjZNHG4QfXOFaS0"
 drive_download(as_id(data_id), path = "airbnb_europe.csv", overwrite = TRUE)
 
-
+#read the airbnb file
+library(readr)
 df <- read_csv("airbnb_europe.csv")
 View(df)
 
