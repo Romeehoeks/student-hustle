50a30444566a50cf4ce3796914b9ac2e41661c3d
Romeehoeks <98810281+Romeehoeks@users.noreply.github.com>
2022-03-15 21:38:37 +0100

    Update clean_data.R
    deleted the top 10 student cities table and loop and added to the analysis

--- src/dataprep/clean_data.R
+++ src/dataprep/clean_data.R
@@ -37,19 +37,3 @@ is.numeric(private_room$price)
 #Finding duplicates
 duplicated(private_room)
 sum(duplicated(private_room))
-
-#Create a table for the top 10 student cities in Europe ascending
-
-Top10_student_cities <- matrix(c("London", "Munich", "Berlin", "Paris", "Zurich", "Vienna", "Edinburgh", "Barcelona","Dublin", "Manchester"), ncol=1, byrow=TRUE)
-colnames(Top10_student_cities) <- c("City")
-rownames(Top10_student_cities) <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-Top10_student_cities <- as.table(Top10_student_cities)
-Top10_student_cities
-
-#Create a loop for the position of the student cities
-cities <- c("London", "Munich", "Berlin", "Paris", "Zurich", "Vienna", "Edinburgh", "Barcelona","Dublin", "Manchester")
-
-for(i in 1:length(cities)) {
-  print(paste(cities[i], "is the number",
-              i, "city in Europe"))
-}
