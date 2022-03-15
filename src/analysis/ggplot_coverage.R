43f557d48e75deacda3dab16ef19cb62d493de90
Romeehoeks <98810281+Romeehoeks@users.noreply.github.com>
2022-03-15 21:41:42 +0100

    Update ggplot_coverage.R

--- src/analysis/ggplot_coverage.R
+++ src/analysis/ggplot_coverage.R
@@ -5,4 +5,5 @@ grafiek <- private_room2 %>%
 
 ggplot(grafiek, aes(x = mean365, y= city, color = city)) + geom_point() + expand_limits(x = 0)
 
+#save the plot to pdf
 ggsave("myplot.availability.pdf")
