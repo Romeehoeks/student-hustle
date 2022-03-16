63635ba11a91d3060a9fa593f558c469b14d458c
Marijekok <m.t.m.kok@tilburguniversity.edu>
2022-03-16 10:35:13 +0100

    European_city_income
    Merged regression + ggplot for Income

--- src/analysis/regression_european_city_income.R
+++ src/analysis/regression_european_city_income.R
@@ -1,6 +1,19 @@
+library(dplyr)
+
 #regression European City - Income (Price * coverage)
 private_room2 <- mutate(private_room, income = price * availability_365)
 View(private_room2)
 
 reg3 = lm( income ~ city, data = private_room2)
 summary(reg3)
+
+#ggplot Income
+grafiek2 <- private_room2 %>%
+  group_by(city)  %>%
+  summarize(meanincome = mean(income))
+
+
+ggplot(grafiek2, aes(x = meanincome, y= city, color = city)) + geom_point()
+
+#save the plot to a pdf
+ggsave("myplot.meanincome.pdf")
