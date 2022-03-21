e72a23a319cdeaea356505096d62493c6585ba3c
Romeehoeks <98810281+Romeehoeks@users.noreply.github.com>
2022-03-21 21:27:58 +0100

    Update position_studentcities.R
    This is the conclusion top 10 student cities

--- src/analysis/position_studentcities.R
+++ src/analysis/position_studentcities.R
@@ -1,16 +1,14 @@
-##Step 4
+##Step 5
 #Create a table for the top 10 student cities in Europe ascending
 
-Top10_student_cities <- matrix(c("London", "Munich", "Berlin", "Paris", "Zurich", "Vienna", "Edinburgh", "Barcelona","Dublin", "Manchester"), ncol=1, byrow=TRUE)
+Top10_student_cities <- matrix(c("Edinburgh", "Manchester", "Munich", "London", "Berlin", "Dublin", "Vienna", "Paris","Barcelona", "Copenhagen"), ncol=1, byrow=TRUE)
 colnames(Top10_student_cities) <- c("City")
 rownames(Top10_student_cities) <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 Top10_student_cities <- as.table(Top10_student_cities)
-Top10_student_cities
 
 #Create a loop for the position of the student cities
-cities <- c("London", "Munich", "Berlin", "Paris", "Zurich", "Vienna", "Edinburgh", "Barcelona","Dublin", "Manchester")
 
-for(i in 1:length(cities)) {
-  print(paste(cities[i], "is the number",
-              i, "city in Europe"))
+for(i in 1:length(Top10_student_cities)) {
+  print(paste(Top10_student_cities[i], "is the number",
+              i, "student city in Europe to rent out your student room"))
 }
