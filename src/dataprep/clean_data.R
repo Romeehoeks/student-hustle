554cbf182c551f604c834d83fd652405d5553014
Romeehoeks <98810281+Romeehoeks@users.noreply.github.com>
2022-03-21 22:44:26 +0100

    Update clean_data.R

--- src/dataprep/clean_data.R
+++ src/dataprep/clean_data.R
@@ -82,6 +82,7 @@ mean_price_cities_euro1 <- mean_price_cities_euro %>%
           mutate(mean_price = case_when(city == 'edinburgh' ~ mean_price / 1.19,
             TRUE ~ as.numeric(mean_price)))
 
+write.csv(filtered_data, 'filtered_data.csv')
 write.csv(private_room, 'private_room.csv')
 write.csv(mean_price_cities_euro1, 'mean_price_cities_euro1.csv')
 
