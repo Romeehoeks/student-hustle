216bebb6b9d97a0ab27c4b66efa8e7f2e318f223
Romeehoeks <98810281+Romeehoeks@users.noreply.github.com>
2022-03-22 23:30:02 +0100

    Update download_files.R
    combine previous step 1 and step 2

--- src/dataprep/download_files.R
+++ src/dataprep/download_files.R
@@ -1,3 +1,16 @@
+#step 1
+#download packages
+install.packages("googledrive")
+install.packages("readr")
+install.packages("tidyverse")
+install.packages("dplyr")
+install.packages("assertive")
+install.packages("stringr")
+install.packages("ggplot2")
+install.packages("‘AggregateR")
+install.packages("effects")
+install.packages("knitr")
+
 #step 2
 library(googledrive)
 library(readr)
