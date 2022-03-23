d669fe573d4eaf75e21de726cc439c4671006637
Romeehoeks <98810281+Romeehoeks@users.noreply.github.com>
2022-03-23 11:03:24 +0100

    Update position_studentcities.R

--- src/analysis/position_studentcities.R
+++ src/analysis/position_studentcities.R
@@ -1,14 +1,16 @@
 ##Last step, this is the conclusion
 #Create a table for the top 10 student cities in Europe ascending
 
-Top10_student_cities <- matrix(c("Edinburgh", "Manchester", "Munich", "London", "Berlin", "Dublin", "Vienna", "Paris","Barcelona", "Copenhagen"), ncol=1, byrow=TRUE)
-colnames(Top10_student_cities) <- c("City")
-rownames(Top10_student_cities) <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-Top10_student_cities <- as.table(Top10_student_cities)
+top10_student_cities <- matrix(c("Edinburgh", "Manchester", "Munich", "London", "Berlin", "Dublin", "Vienna", "Paris","Barcelona", "Copenhagen"), ncol=1, byrow=TRUE)
+colnames(top10_student_cities) <- c("City")
+rownames(top10_student_cities) <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+top10_student_cities <- as.table(top10_student_cities)
 
 #Create a loop for the position of the student cities
 
-for(i in 1:length(Top10_student_cities)) {
-  print(paste(Top10_student_cities[i], "is the number",
+for(i in 1:length(top10_student_cities)) {
+  print(paste(top10_student_cities[i], "is the number",
               i, "student city in Europe to rent out your student room"))
 }
+
+write.csv(top10_student_cities, "../../gen/dataprep/output/top10_student_cities.csv", row.names = FALSE)
