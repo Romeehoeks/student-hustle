25c0e7cfcc0d7e9817369de8c1815702acc5ee73
Thomas-H1999 <98875680+Thomas-H1999@users.noreply.github.com>
2022-03-23 12:41:58 +0100

    Update plot_means.R

--- src/analysis/plot_means.R
+++ src/analysis/plot_means.R
@@ -11,21 +11,27 @@ library(broom)
 
 combined_mean_data1 <- read.csv(file = '../../gen/dataprep/input/combined_mean_data1.csv')
 
+#create directions
+
+dir.create('../../gen/analysis')
+dir.create('../../gen/analysis/data')
+dir.create('../../gen/analysis/output')
+
 #plot the mean prices. As you can see the price in dublin is the highest, and in manchester the lowest.
 
 ggplot(combined_mean_data1, aes(x= city, y=mean_price, color = city)) +
   geom_point() 
-ggsave("mean_price.pdf")
+ggsave("../../gen/analysis/output/mean_price.pdf")
 
 #plot the mean reviews per city. As you can see the highest score is in edinburgh, the lowest is in barcelona
 
 ggplot(combined_mean_data1, aes(x= city, y=mean_reviews1, color = city)) +geom_point()
-ggsave("mean_reviews.pdf")
+ggsave("../../gen/analysis/output/mean_reviews.pdf")
 
 #plot the mean availability per city. 
 
 ggplot(combined_mean_data1, aes(x= city, y=mean_availability, color = city)) +geom_point()
-ggsave("mean_availability.pdf")
+ggsave("../../gen/analysis/output/mean_availability.pdf")
 
 dir.create('../../gen/analysis')
 dir.create('../../gen/analysis/data')
